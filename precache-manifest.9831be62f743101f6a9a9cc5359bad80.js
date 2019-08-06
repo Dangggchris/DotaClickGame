9c43d687e6d432ddde1557b37f041a32117c0d15
Dangggchris <christophermtdang@gmail.com>
2019-08-06 01:25:42 -0700

    Updates

---
+++ precache-manifest.9831be62f743101f6a9a9cc5359bad80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d67d573323bd29f1a2338f2f100b87",
+    "url": "/DotaClickGame/index.html"
+  },
+  {
+    "revision": "0d714f808ad145d86854",
+    "url": "/DotaClickGame/static/css/main.8fbcdcbc.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/DotaClickGame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "0d714f808ad145d86854",
+    "url": "/DotaClickGame/static/js/main.7bc973ef.chunk.js"
+  },
+  {
+    "revision": "607684b96de1627dc324",
+    "url": "/DotaClickGame/static/js/runtime~main.b5e4988a.js"
+  }
+]);
